10a2108b98351223c983ec097c8f89b3bd159d4b
p4535992 <tentimarco0@gmail.com>
2023-09-02 23:51:57 +0200

    upgrade v11

--- vite.config.mjs
+++ vite.config.mjs
@@ -17,7 +17,7 @@ import { run } from 'vite-plugin-run'
 
 // For convenience, you just need to modify the package ID below as it is used to fill in default proxy settings for
 // the dev server.
-const s_MODULE_ID = "magic-items-3";
+const s_MODULE_ID = "safety-tools";
 const s_PACKAGE_ID = "modules/"+s_MODULE_ID;
 const s_ENTRY_JAVASCRIPT = "module.js";
 
